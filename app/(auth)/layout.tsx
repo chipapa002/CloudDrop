72a39b0bfb95b6d1bd7efa0d04d058c4a51633d5
Nndamulele Casper Tshipapa <nndamulelechip18@gmail.com>
2024-12-28 04:07:28 +0200

    build errors

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="relative flex min-h-screen">
-
-            <div className="absolute left-4 top-4 z-10">
+        <div className="flex min-h-screen">
+            <div className="absolute top-4 left-4 z-10">
                 <Image
                     src="/assets/images/2.png"
-                    alt="logo"
+                    alt="illustration"
                     width={130}
-                    height={100}
-                    // className="h-auto w-auto"
+                    height={130}
+                    className="transition-all hover:rotate-2 hover:scale-105 hidden lg:block"
                 />
             </div>
 
-            {/* Left section with a responsive background */}
+
+            {/* Left section with a background image */}
             <section
-                className="hidden w-1/2 items-center justify-center bg-brand bg-[url('/assets/images/background-left.png')] bg-fixed bg-left p-10 lg:flex xl:w-2/5"
+                className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5 bg-[url('/assets/images/background-left.png')] bg-fixed bg-left"
             >
                 <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
 
@@ -29,10 +29,19 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
                 </div>
             </section>
 
-            {/* Right section with a responsive background */}
+            {/* Right section with a background image */}
             <section
-                className="flex flex-1 flex-col items-center bg-white bg-[url('/assets/images/background-right.jpg')] bg-cover bg-center p-4 py-10 lg:justify-center lg:p-10 lg:py-0"
+                className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0 bg-[url('/assets/images/background-right.jpg')] bg-cover bg-center"
             >
+                <div className="mb-16 lg:hidden">
+                    <Image
+                        src="/assets/images/Logo-full.png"
+                        alt="logo"
+                        width={200}
+                        height={50}
+                        className="h-auto w-[200px] lg:w-[250px]"
+                    />
+                </div>
                 {children}
             </section>
         </div>
